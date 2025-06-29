028361771d65a9aed2732143a24c223d786df634
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-06-29 11:45:12 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,39 @@
+variable "region" {
+  description = "region"
+  default     = "ap-northeast-2"
+}
 
+variable "db_name" {
+  description = "DB name"
+  default     = "cafe_db"
+}
+
+variable "db_username" {
+  description = "master user"
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "master password"
+  sensitive   = true
+  default     = "password"
+}
+
+variable "domain_name" {
+  description = "Domain name"
+  default     = "rainhyeon.store"
+}
+
+// Filename: terraform.tf
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
